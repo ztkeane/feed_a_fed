e80f623d1cba7d72aff773efa99ce9340e4eadcb
Zachary Keane <zach@dhcp-10-132-165-241.uawifi.arizona.edu>
2019-01-19 13:36:39 -0700

    New classes.

---
+++ faf/src/main/java/hello/HelpCenter.java
@@ -0,0 +1,39 @@
+package hello;
+
+public class HelpCenter {
+    
+    private String aud;
+    private String cid;
+    private String name;
+    private String address;
+    private String city;
+    private String county;
+    private String state;
+    private String phoneNo;
+    
+    
+    public String getAud() { return aud; }
+    public void setAud(String aud) { this.aud = aud; }
+    
+    public String getCid() { return eid; }
+    public void setCid(String eid) { this.eid = eid; }
+    
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    
+    public String getAddress() { return address; }
+    public void setAddress(String email) { this.address = address; }
+    
+    public String getCity() { return city; }
+    public void setCity(String city) {this.city = city; }
+    
+    public String getCounty() { return county; }
+    public void setCounty(String county) { this.county = county; }
+    
+    public String getState() { return state; }
+    public void setState(String state) { this.state = state; }
+    
+    public String getPhoneNo() { return phoneNo; }
+    public void setPhoneNo(String phoneNo) { this.phoneNo = phoneNo; }
+    
+}
---
+++ faf/src/main/java/hello/Location.java
@@ -0,0 +1,23 @@
+package hello;
+
+public class Location {
+    
+    private String aud;
+    private String city;
+    private String county;
+    private String state;
+    
+    
+    public String getAud() { return aud; }
+    public void setAud(String aud) { this.aud = aud; }
+    
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+    
+    public String getCounty() { return county; }
+    public void setCounty(String county) { this.county = county; }
+    
+    public String getState() { return state; }
+    public void setState(String state) { this.state = state; }
+    
+}
