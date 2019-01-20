fa2b2b7c3ed62bfcc9d3d6d6dcb494c826f5e7d2
Sureshdevendran <sureshkrishna.mw@gmail.com>
2019-01-19 23:58:06 -0700

    finished

--- faf/src/main/java/hello/QueryController.java
+++ faf/src/main/java/hello/QueryController.java
@@ -94,4 +94,91 @@ public class QueryController {
         return "query1Result";
     }
     
+    /******************************************************
+     * QUERY #2
+     ******************************************************/
+    
+    @GetMapping("/query2")
+    public String query2Form(Model model) {
+        model.addAttribute("query2", new Query2());
+        return "query2";
+    }
+    
+    /*------------------------------------------------------------------------*
+     |          Method:  query1Submit
+     |         Purpose:  To perform actions according to this query.
+     |  Pre-Conditions:  The database exists and can be queried.
+     | Post-Conditions:  The query is given to the user.
+     |      Parameters:  Model, Query1
+     |         Returns:  String, the HTML page.
+     *------------------------------------------------------------------------*/
+    @PostMapping("/query2")
+    public String query2Submit(Model model, @ModelAttribute Query2 query) {
+        
+        String sqlQuery1 = "SELECT p1.cid, p1.name, p1.address, p1.phoneNo, p1.city, p1.county, p1.state "
+        + "FROM ztkeane.helpCenter p1 "
+        + "WHERE (p1.city='"+query.getCity()+"' OR p1.county='"+query.getCounty()+"' ) AND p1.state='"+query.getState()+"' ";
+        
+        List<Query2Result> allData = this.jdbcTemplate.query(sqlQuery2,
+                                                             new RowMapper<Query2Result>() {
+                                                                 public Query2Result mapRow(ResultSet rs, int rowNum) throws SQLException {
+                                                                     Query2Result result = new Query2Result();
+                                                                     result.setCid(rs.getString("cid"));
+                                                                     result.setName(rs.getString("name"));
+                                                                     result.setAddress(rs.getString("address"));
+                                                                     result.setCity(rs.getString("city"));
+                                                                     result.setCounty(rs.getString("county"));
+                                                                     result.setState(rs.getString("state"));
+                                                                     result.setPhoneNo(rs.getString("phoneNo"));
+                                                                     return result;
+                                                                 }
+                                                             });
+        model.addAttribute("query2", query);
+        model.addAttribute("data", allData);
+        return "query2Result";
+    }
+    
+    /******************************************************
+     * QUERY #3
+     ******************************************************/
+    
+    @GetMapping("/query3")
+    public String query3Form(Model model) {
+        model.addAttribute("query3", new Query3());
+        return "query3";
+    }
+    
+    /*------------------------------------------------------------------------*
+     |          Method:  query1Submit
+     |         Purpose:  To perform actions according to this query.
+     |  Pre-Conditions:  The database exists and can be queried.
+     | Post-Conditions:  The query is given to the user.
+     |      Parameters:  Model, Query1
+     |         Returns:  String, the HTML page.
+     *------------------------------------------------------------------------*/
+    @PostMapping("/query3")
+    public String query3Submit(Model model, @ModelAttribute Query3 query) {
+        
+        String sqlQuery3 = "SELECT p1.cid, p1.name, p1.address, p1.phoneNo, p1.city, p1.county, p1.state "
+        + "FROM ztkeane.helpCenter p1 "
+        + "WHERE (p1.city='"+query.getCity()+"' OR p1.county='"+query.getCounty()+"' ) AND p1.state='"+query.getState()+"' ";
+        
+        List<Query3Result> allData = this.jdbcTemplate.query(sqlQuery3,
+                                                             new RowMapper<Query3Result>() {
+                                                                 public Query3Result mapRow(ResultSet rs, int rowNum) throws SQLException {
+                                                                     Query3Result result = new Query3Result();
+                                                                     result.setCid(rs.getString("cid"));
+                                                                     result.setName(rs.getString("name"));
+                                                                     result.setAddress(rs.getString("address"));
+                                                                     result.setCity(rs.getString("city"));
+                                                                     result.setCounty(rs.getString("county"));
+                                                                     result.setState(rs.getString("state"));
+                                                                     result.setPhoneNo(rs.getString("phoneNo"));
+                                                                     return result;
+                                                                 }
+                                                             });
+        model.addAttribute("query3", query);
+        model.addAttribute("data", allData);
+        return "query3Result";
+    }
 }
