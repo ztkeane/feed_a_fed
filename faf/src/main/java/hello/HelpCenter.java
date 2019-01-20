6e7639fb4d88c26a92627ea5097d9ba711bf5e62
Zachary Keane <zach@dhcp-10-132-165-241.uawifi.arizona.edu>
2019-01-19 23:45:14 -0700

    Hope

--- faf/src/main/java/hello/HelpCenter.java
+++ faf/src/main/java/hello/HelpCenter.java
@@ -22,7 +22,7 @@ public class HelpCenter {
     public void setName(String name) { this.name = name; }
     
     public String getAddress() { return address; }
-    public void setAddress(String email) { this.address = address; }
+    public void setAddress(String address) { this.address = address; }
     
     public String getCity() { return city; }
     public void setCity(String city) {this.city = city; }
